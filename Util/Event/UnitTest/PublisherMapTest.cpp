daddf907fda47e5c8c7023b48353e40d07ac50d6
andywang0607 <andywang0607@gmail.com>
2022-08-08 00:15:51 +0800

    [feat] Implement PublisherMap
    - When emplace new item to map, function in topic_will be triggered

---
+++ Util/Event/UnitTest/PublisherMapTest.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <string>
+
+#include "PublisherMap.hpp"
+
+using namespace Util::Event;
+
+TEST(PublisherMapTest, BasicPublishTest)
+{
+    PublisherMap<std::string, int> map;
+    Unsubscriber unsubcriber;
+
+    int count = 0;
+    bool ret = 0;
+
+    std::string lastKey = "USTETH";
+    int lastValue = 800;
+
+    unsubcriber += map.subscribe([&](auto &key, auto &value) {
+        ++count;
+        EXPECT_EQ(key, lastKey);
+        EXPECT_EQ(value, lastValue);
+    });
+
+    map.emplace(std::piecewise_construct, std::forward_as_tuple(lastKey), std::forward_as_tuple(lastValue));
+    EXPECT_EQ(count, 1);
+};
