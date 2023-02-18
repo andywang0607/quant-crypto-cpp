e58a67e15073903d10b330054054058b93ced453
YiChih Wang <andywang0607@gmail.com>
2023-02-18 21:58:44 +0800

    [refactor] Apply RestRequester to TradeAdapter

--- Trade/BybitTradeAdapter/BybitSpotTradeAdapter.hpp
+++ Trade/BybitTradeAdapter/BybitSpotTradeAdapter.hpp
@@ -2,6 +2,7 @@
 #define __BYBITSPOTTRADEADAPTER_H__
 
 #include "BybitSignTool.hpp"
+#include "RestRequester.hpp"
 #include "TimeUtils.hpp"
 #include "TradeNode.hpp"
 #include "TradeAdapter.hpp"
@@ -11,7 +12,6 @@
 #include <string>
 
 #include <nlohmann/json.hpp>
-#include <restclient-cpp/restclient.h>
 
 using namespace QuantCrypto::Trade;
 using namespace Util::Sign;
@@ -31,51 +31,63 @@ public:
     bool createOrder(Order *order)
     {
         static const std::string Path = "spot/v1/order";
-
         static std::map<std::string, std::string> params{
             {"api_key", config_["exchange"]["bybit"]["apiKey"].get<std::string>()}};
+        
+        return Util::Requester::RestRequester::post(
+            [this, &order]() {
+                params["symbol"] = order->symbol_;
+                params["qty"] = std::to_string(order->qty_);
+                params["price"] = std::to_string(order->price_);
+                params["timestamp"] = std::to_string(Util::Time::getTime());
+                params["side"] = [order]() -> std::string {
+                    if (order->side_ == Side::Buy) {
+                        return "BUY";
+                    }
+                    return "SELL";
+                }();
+                params["type"] = [&order]() {
+                    if (order->type_ == OrderType::Limit) {
+                        return "LIMIT";
+                    }
+                    if (order->type_ == OrderType::Market) {
+                        return "MARKET";
+                    }
+                    return "LIMIT_MAKER";
+                }();
+                params["timeInForce"] = [order]() {
+                    if (order->timeInForce_ == TimeinForce::GTC) {
+                        return "GTC";
+                    }
+                    if (order->timeInForce_ == TimeinForce::FOK) {
+                        return "FOK";
+                    }
+                    return "IOC";
+                }();
+                order->customOrderId_ = genOrderId(order->symbol_);
+                params["orderLinkId"] = order->customOrderId_;
 
-        params["symbol"] = order->symbol_;
-        params["qty"] = std::to_string(order->qty_);
-        params["price"] = std::to_string(order->price_);
-        params["timestamp"] = std::to_string(Util::Time::getTime());
-        params["side"] = [order]() -> std::string {
-            if (order->side_ == Side::Buy) {
-                return "BUY";
-            }
-            return "SELL";
-        }();
-        params["type"] = [&order]() {
-            if (order->type_ == OrderType::Limit) {
-                return "LIMIT";
-            }
-            if (order->type_ == OrderType::Market) {
-                return "MARKET";
-            }
-            return "LIMIT_MAKER";
-        }();
-        params["timeInForce"] = [order]() {
-            if (order->timeInForce_ == TimeinForce::GTC) {
-                return "GTC";
-            }
-            if (order->timeInForce_ == TimeinForce::FOK) {
-                return "FOK";
-            }
-            return "IOC";
-        }();
-        order->customOrderId_ = genOrderId(order->symbol_);
-        params["orderLinkId"] = order->customOrderId_;
-
-        const auto signQueryString = BybitSignTool::signHttpReq(params, apiSecret_);
-        const auto request = URL + Path + "?" + signQueryString;
-
-        RestClient::Response r = RestClient::post(request, "application/x-www-form-urlencoded", "");
-        if (r.code != 200) {
-            logger_.warn("createOrder failed, request={} r.code={}, r.body={}", request, r.code, r.body);
-            return false;
-        }
-
-        return true;
+                const auto signQueryString = BybitSignTool::signHttpReq(params, apiSecret_);
+                const auto request = URL + Path + "?" + signQueryString;
+                
+                return request;
+            },
+            []() {
+                return "application/x-www-form-urlencoded";
+            },
+            []() {
+                return "";
+            },
+            [](const auto &response) {
+                if (response.code != 200) {
+                    return false;
+                }
+                const nlohmann::json result = nlohmann::json::parse(response.body);
+                if (result["ret_code"].get<int>() != 0 || result["ext_code"].get<std::string>() != "") {
+                    return false;
+                }
+                return true;
+            });
     }
 
     bool deleteOrder(Order *order)
@@ -84,19 +96,26 @@ public:
         static std::map<std::string, std::string> params{
             {"api_key", config_["exchange"]["bybit"]["apiKey"].get<std::string>()}};
 
-        params["orderLinkId"] = order->customOrderId_;
-        params["timestamp"] = std::to_string(Util::Time::getTime());
+        return Util::Requester::RestRequester::del(
+            [this, &order]() {
+                params["orderLinkId"] = order->customOrderId_;
+                params["timestamp"] = std::to_string(Util::Time::getTime());
 
-        const auto signQueryString = BybitSignTool::signHttpReq(params, apiSecret_);
-        const auto request = URL + Path + "?" + signQueryString;
+                const auto signQueryString = BybitSignTool::signHttpReq(params, apiSecret_);
+                const auto request = URL + Path + "?" + signQueryString;
 
-        RestClient::Response r = RestClient::del(request);
-        if (r.code != 200) {
-            logger_.warn("deleteOrder failed, request={} r.code={}, r.body={}", request, r.code, r.body);
-            return false;
-        }
-
-        return true;
+                return request;
+            },
+            [](const auto &response) {
+                if (response.code != 200) {
+                    return false;
+                }
+                const nlohmann::json result = nlohmann::json::parse(response.body);
+                if (result["ret_code"].get<int>() != 0 || result["ext_code"].get<std::string>() != "") {
+                    return false;
+                }
+                return true;
+            });
     }
 
     bool queryWallet()
@@ -105,19 +124,32 @@ public:
         static std::map<std::string, std::string> params{
             {"api_key", config_["exchange"]["bybit"]["apiKey"].get<std::string>()}};
 
-        params["timestamp"] = std::to_string(Util::Time::getTime());
+        nlohmann::json respBody;
+        if (!Util::Requester::RestRequester::get(
+                [this]() {
+                    params["timestamp"] = std::to_string(Util::Time::getTime());
 
-        const auto signQueryString = BybitSignTool::signHttpReq(params, apiSecret_);
-        const auto request = URL + Path + "?" + signQueryString;
+                    const auto signQueryString = BybitSignTool::signHttpReq(params, apiSecret_);
+                    const auto request = URL + Path + "?" + signQueryString;
 
-        RestClient::Response r = RestClient::get(request);
-        if (r.code != 200) {
-            logger_.warn("queryWallet failed, request={} r.code={}, r.body={}", request, r.code, r.body);
+                    return request;
+                },
+                [&respBody](const auto &response) {
+                    if (response.code != 200) {
+                        return false;
+                    }
+                    const nlohmann::json result = nlohmann::json::parse(response.body);
+                    if (result["ret_code"].get<int>() != 0 || result["ext_code"].get<std::string>() != "") {
+                        return false;
+                    }
+                    respBody = result;
+                    return true;
+                })) {
             return false;
         }
 
-        const auto result = nlohmann::json::parse(r.body)["result"]["balances"];
-        for (const auto &coinObj : result) {
+        const auto balances = respBody["result"]["balances"];
+        for (const auto &coinObj : balances) {
             const std::string coin = coinObj["coin"].get<std::string>();
             auto &coinPosition = wallet_[coin];
 
@@ -132,7 +164,7 @@ public:
     }
 
 private:
-    inline std::string genOrderId(const std::string &symbol)
+    static inline std::string genOrderId(const std::string &symbol)
     {
         static long long sequenceNo = 0;
         return std::string("Bybit") + Util::Time::getDateTime() + symbol + std::to_string(sequenceNo++);
